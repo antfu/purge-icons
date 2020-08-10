b43addb394393420027176f492289f0b498fb857
Anthony Fu <anthonyfu117@hotmail.com>
2020-08-10 16:28:48 +0800

    chore: update cli

--- packages/purge-icons/scripts/prepare.ts
+++ packages/purge-icons/scripts/prepare.ts
@@ -2,6 +2,9 @@ import collections from '@iconify/json/collections.json'
 import fs from 'fs-extra'
 
 (async() => {
+  for (const f of ['README.md', 'LICENSE'])
+    await fs.copyFile(`../../${f}`, `./${f}`)
+
   await fs.ensureDir('./src/generated')
   await fs.writeFile('./src/generated/collections.ts', `
 export type CollectionId = ${Object.keys(collections).map(i => `'${i}'`).join(' | ')}
